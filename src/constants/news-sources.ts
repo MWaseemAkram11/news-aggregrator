a914a37efbba168fc8e12eb065c2e90d31969db6
Muhammad Waseem Akram <mwaseemakram335@gmail.com>
2025-03-19 00:50:01 +0500

    chore: news sources categories.

---
+++ src/constants/news-sources.ts
@@ -0,0 +1,17 @@
+export const SOURCES = [
+    { id: "newsapi", name: "NewsAPI" },
+    { id: "guardian", name: "The Guardian" },
+    { id: "nyt", name: "New York Times" }
+  ];
+  
+  export const CATEGORIES = [
+    { id: "business", name: "Business" },
+    { id: "technology", name: "Technology" },
+    { id: "entertainment", name: "Entertainment" },
+    { id: "health", name: "Health" },
+    { id: "science", name: "Science" },
+    { id: "sports", name: "Sports" },
+    { id: "politics", name: "Politics" },
+    { id: "environment", name: "Environment" },
+    { id: "arts", name: "Arts" }
+  ];
