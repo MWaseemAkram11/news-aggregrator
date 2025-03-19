4a00869c4c638130a3acfdaa1ab693e8ca2c63f5
Muhammad Waseem Akram <mwaseemakram335@gmail.com>
2025-03-19 17:53:57 +0500

    ref: refactoring the app.tsx file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { NewsProvider } from './context/news-context';
 import Home from './pages/Home';
+import { Toaster } from './components/ui/toaster';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <NewsProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Toaster />
+      </Router>
+    </NewsProvider>
   );
 }
 
