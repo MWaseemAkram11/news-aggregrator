1bbd5334bf21c24cf9f33d04dc5527e47ec136ce
Muhammad Waseem Akram <mwaseemakram335@gmail.com>
2025-03-19 20:26:19 +0500

    chore: added footer.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,13 +1,12 @@
-
 const footer = () => {
-  return (
-    <div className="bg-white border-t border-gray-200 py-4 text-center text-gray-600 text-sm">
-        <div className="flex justify-center items-center">
-          <span>Powered by</span>
-          <small>@ News Aggregrator All Rights reserved</small>
-        </div>
-      </div>
-  )
-}
-
+    return (
+      <div className="bg-white border-t border-gray-200 py-4 text-center text-gray-600 text-sm">
+          <div className="flex justify-center items-center">
+            <span>Powered by</span>
+            <small>@ News Aggregrator All Rights reserved</small>
+          </div>
+        </div>
+    )
+  }
+  
 export default footer
